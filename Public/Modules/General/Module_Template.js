04e29f644baea2e66dfe0254654f3a7fe3ac7c77
Chenyang Wei <ChenyangWei.CWei@gmail.com>
2023-09-25 10:14:51 -0400

    Add the script and module templates.

---
+++ Public/Modules/General/Module_Template.js
@@ -0,0 +1,52 @@
+/** 
+ * Introduction:
+ *  The objects and funcitons in this module are defined for
+ *    performing ....
+ * 
+ * Object groups:
+ *  1) Group 1: ....
+ * 
+ * Function groups:
+ *  1) Group 1: ....
+ * 
+ * Reference to the public repository:
+ *  https://code.earthengine.google.com/?scriptPath=users/ChenyangWei/Public
+ * 
+ * Load this module:
+ *  var LDM = require("users/ChenyangWei/Public:Modules/General/LANDSAT_Data_Manipulation.js");
+ * 
+ * Update: 9/22/2023.
+ */
+
+
+/****** Prerequisite module(s). ******/
+
+
+
+/****** Objects. ******/
+
+
+/**** 1) Group 1: .... ****/
+
+/* Object 1. */
+exports.Object_1;
+
+
+
+/****** Functions. ******/
+
+
+/**** 1) Group 1: .... ****/
+
+/**
+ * Function's purpose
+ * 
+ * @param {param_1_type} param_1_name - description
+ * @param {param_2_type} param_2_name - description
+ * 
+ * @return {return_type} - description
+ */
+exports.Function_Name = function(param_1, param_2) {
+  return return_Value;
+};
+
---
+++ Public/Modules/General/Script_Template.js
@@ -0,0 +1,50 @@
+/**
+ * Introduction:
+ * 
+ *  1) Operation #1.
+ * 
+ *  2) Operation #2.
+ * 
+ *  3) Operation #3.
+ * 
+ * Updated: 9/21/2023.
+ * 
+ * Runtime: N/A.
+*/
+
+
+/* Module loading. */
+
+
+/* Object definition. */
+
+
+/* Function definition. */
+
+
+/* Dataset loading. */
+
+
+/* 1) Operation #1. */
+
+
+/* 2) Operation #2. */
+
+
+/* 3) Operation #3. */
+
+
+var output = false;
+
+if (!output) {
+  
+  /* Check the result(s). */
+  
+  Map.setOptions("Satellite");
+  
+} else {
+  
+  /* Output the result(s).*/
+  
+}
+
